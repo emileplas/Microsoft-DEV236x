4be06fc7bda7a4a229f1b90e1b1182bafd136b16
Siddharth Kumaran <siddgood@gmail.com>
2017-07-16 18:19:02 -0500

    Update MOD03_1-4.3_Intro_Python.py

--- MOD03_1-4.3_Intro_Python.py
+++ MOD03_1-4.3_Intro_Python.py
@@ -1,6 +1,14 @@
-#tf_quiz function
+'''
+tf_quiz function
 
-# [ ] Create the program, run tests
+tf_quiz() has 2 parameters which are both string arguments
+
+question: a string containg a T/F question like "Should save your notebook after edit?(T/F): "
+
+correct_ans: a string indicating the correct answer, either "T" or "F"
+
+tf_quiz() returns a string: "correct" or "incorrect"
+'''
 
 def tf_quiz(question, correct_ans):
     ent_ans = input(question).upper()
