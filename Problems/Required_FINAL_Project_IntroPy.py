b9193284ae115c1b2318c1e1d011b4e9d0771a52
Siddharth Kumaran <siddgood@gmail.com>
2017-07-22 18:53:30 -0500

    Update Required_FINAL_Project_IntroPy.py

--- Problems/Required_FINAL_Project_IntroPy.py
+++ Problems/Required_FINAL_Project_IntroPy.py
@@ -22,3 +22,28 @@ def adding_report(choice):
         print("Total\n", total)
         
 adding_report("A")
+
+# in progress
+
+def adding_report(choice):
+    total = 0
+    i = 0
+    items = ""
+    user_input = ""
+    while user_input.isalpha() == False:
+        while (user_input.upper() == "Q") or (user_input.upper().startswith("Q") == True) or (user_input.isdigit()):
+            user_input = input("Enter an integer or 'Q': ")
+            if (user_input.lower() == "q") or (user_input.lower().startswith("q") == True):
+                break
+            else:
+                items = items + "\n" + user_input
+                total = total + int(user_input)
+                i = i + 1
+            
+    if choice == "A":
+        print("Items\n", items) 
+        print("Total\n", total) 
+    elif choice=="T": 
+        print("Total\n", total)
+
+adding_report("T")
